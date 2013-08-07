f08953e2eacc6c4313ed8b97c7958ffdfede509a
Dwayne Charrington <dwayencharrington@gmail.com>
2013-08-07 12:10:38 +1000

    Empty alias class.

---
+++ classes/Theme.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Theming functionality for your Kohana application
+ *
+ * @package    Theme
+ * @author     Dwayne Charrington
+ * @copyright  (c) 2013 Dwayne Charrington + Contributors
+ * @license    http://opensource.org/licenses/BSD-3-Clause
+ */
+
+abstract class Theme extends Kohana_Theme {  }
