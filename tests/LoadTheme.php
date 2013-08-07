79d93a37d8a9322b1d5af8e34b1d76c26fc7a2b8
Dwayne Charrington <dwayencharrington@gmail.com>
2013-08-07 12:11:53 +1000

    The first of many unit tests for this module.

---
+++ tests/LoadTheme.php
@@ -0,0 +1,37 @@
+<?php defined('SYSPATH') OR die('Kohana bootstrap needs to be included before tests run');
+
+class LoadTheme extends Unittest_TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        // $this->markTestSkipped("Message");
+    }
+
+    /**
+     * Test Load Actual Theme
+     *
+     * Simulate loading an actual theme
+     *
+     */
+    public function testLoadActualTheme()
+    {
+        $key = Keys::Generate();
+
+        $this->assertNotEquals(FALSE, $key);
+    }
+
+    /**
+     * Test Load Nonexistent Theme
+     *
+     * Test what happens when a theme does not exist
+     *
+     * @expectedException               Kohana_Exception
+     * @expectedExceptionMessage did not contain your replace character
+     *
+     */
+    public function testGenerateFormatWithoutReplaceChar()
+    {
+        $key = Keys::Generate("hdjas-skajdkasj-dj938-8848");
+    }
+}
