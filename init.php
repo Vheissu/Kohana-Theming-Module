e2f2244e1eab86cd5b4ae3adcd665f8c66b78e30
Dwayne Charrington <dwayencharrington@gmail.com>
2013-08-07 13:15:28 +1000

    Config loading should be handled within the constructor.

--- init.php
+++ init.php
@@ -1,8 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-// Get all config options
-Theme::$_config = Kohana::$config->load("theme");
-
 // Create an instance of the Theme library
 $theme = Theme::Factory();
 
