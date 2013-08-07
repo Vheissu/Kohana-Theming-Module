5c4b5e6d8b601aa27e9b7310647c306f688dfa46
Dwayne Charrington <dwayencharrington@gmail.com>
2013-08-07 12:12:50 +1000

    Theme index.php file, does nothing yet.

---
+++ themes/default/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title><?php echo $title; ?></title>
+    </head>
+    <body>
+        <?php echo $content; ?>
+    </body>
+</html>
