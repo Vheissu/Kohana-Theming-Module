4bf8842d95d3bf5e12628a2b6a832119c65bb671
Dwayne Charrington <dwayencharrington@gmail.com>
2013-08-07 12:10:51 +1000

    Theme module config with some default paths.

---
+++ config/theme.php
@@ -0,0 +1,36 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Theming functionality for your Kohana application
+ *
+ * @package    Theme
+ * @author     Dwayne Charrington
+ * @copyright  (c) 2013 Dwayne Charrington + Contributors
+ * @license    http://opensource.org/licenses/BSD-3-Clause
+ */
+
+return array(
+
+    /**
+     * Theme locations.
+     *
+     * There are 3 possible default locations to search from
+     * you can add or remove as you see fit.
+     *
+     * Order each directory in importance. The first directory
+     * if the requested theme is found will have priority over any
+     * of the other folders which are fallbacks.
+     *
+     * If the requested theme for example 'default' does not exist
+     * in the module folder 'themes' then the theme will be searched
+     * for in the root directory and finally the application directory
+     * before failing completely.
+     *
+     */
+    "theme_dirs" = > array(
+        "module_dir" => MODPATH.'theme' . DIRECTORY_SEPARATOR . 'themes',
+        "root_dir"      => DOCROOT.'themes',
+        "app_dir"       => APPPATH.'themes',
+    );
+
+);
